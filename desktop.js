fc97ca72ab4474e71d136c8f1527072aa0cb2e81
Kaspar <kylgorvigajava@gmail.com>
2025-03-16 14:26:31 +0200

    added client cards and view

--- desktop.js
+++ desktop.js
@@ -1,6 +1,8 @@
 var browserModal = document.getElementById("browserModal");
 var openBrowser = document.getElementById("openBrowser");
 var closeBrowser = document.getElementById("closeBrowser");
+var clientModal = document.getElementById("clientModal");
+var closeClientModal = document.getElementById("closeClientModal");
 
 openBrowser.onclick = function () {
     browserModal.style.display = "block";
@@ -15,6 +17,30 @@ window.onclick = function (event) {
         browserModal.style.display = "none";
     }
 }
+
+closeClientModal.onclick = function () {
+    clientModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == clientModal) {
+        clientModal.style.display = "none";
+    }
+}
+
+var cards = document.querySelectorAll(".card");
+cards.forEach(function (card) {
+    card.onclick = function () {
+        var clientName = card.getAttribute("data-client");
+        var clientImage = card.getAttribute("data-image");
+        document.getElementById("clientModalTitle").textContent = clientName;
+        document.getElementById("clientModalBody").innerHTML = `
+            <p>Details for ${clientName}</p>
+            <img src="${clientImage}" class="img-fluid" alt="${clientName}">
+        `;
+        clientModal.style.display = "block";
+    }
+});
 
 function updateDateTime() {
     const now = new Date();
